b9134eeff5af5b1e61dc4fdd8c1ca541d4175303
SlavaLungu <NotYet>
2023-04-19 00:04:31 +0300

    New commit

--- MatrixCalculationProgram.py
+++ MatrixCalculationProgram.py
@@ -3,6 +3,20 @@ def print_matrix(data_matrix):
     for j in range(len(data_matrix[0])):
       print(data_matrix[i][j], end=" ")
     print("")
+2
+
+def matrix_sum(matr_1, matr_2):
+  new_matrix=[]
+  if len(matr_1) == len(matr_2) and len(matr_1[0]) == len(matr_2):
+    for i in range(len(matr_1)):
+      new_matrix.append([])
+      for j in range(len(matr_2)):
+        new_matrix[i].append(matr_1[i][j]+matr_2[i][j])
+    
+  return new_matrix
+3
+3
+
 
 
 # первая матрица
@@ -33,17 +47,21 @@ for i in range(n_2):
       buf_stroka.append(0)
   matrix_2.append(buf_stroka)
 matrix_res=[]
-if(m_1 == n_2):
-  for i in range(n_1):
-    buf_stroka=[]
-    for j in range(m_2):
-      buf_res=0
-      for k in range(m_1):
-        buf_res+=matrix_1[i][k]*matrix_2[k][j]
-      buf_stroka.append(buf_res)
-    matrix_res.append(buf_stroka)  
 
-else:
-  print("Нельзя перемножать")
 
-print_matrix(matrix_res)
+
+
+#if(m_1 == n_2):
+#  for i in range(n_1):
+#    buf_stroka=[]
+#    for j in range(m_2):
+#      buf_res=0
+#      for k in range(m_1):
+#        buf_res+=matrix_1[i][k]*matrix_2[k][j]
+#      buf_stroka.append(buf_res)
+#    matrix_res.append(buf_stroka)  
+
+#else:
+#  print("Нельзя перемножать")
+
+print_matrix(matrix_sum(matrix_1,matrix_2))
